afd339e3939c5db0ea8c3ea7bcc96db32d6b863b
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-08-08 00:41:33 +0900

    test

--- scripts/allbuild.sh
+++ scripts/allbuild.sh
@@ -6,7 +6,8 @@ for dir in $(find slides -type d); do
   for subdir in $(find $dir/. -type d); do
     echo "==== $subdir ===="
     cd "$dir/$subdir"
-    bun install
-    bunx slidev -y -o ../../output/$dir/$subdir --base "/$dir/$subdir" --download true
+    ls
+    #bun install
+    #bunx slidev -y -o ../../output/$dir/$subdir --base "/$dir/$subdir" --download true
   done
 done
