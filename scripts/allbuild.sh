bb94b60e5bf1a5256fa724d415938c65b3a8dca5
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-08-08 00:42:09 +0900

    Fix build process by updating script execution in GitHub Actions workflow

--- scripts/allbuild.sh
+++ scripts/allbuild.sh
@@ -6,8 +6,7 @@ for dir in $(find slides -type d); do
   for subdir in $(find $dir/. -type d); do
     echo "==== $subdir ===="
     cd "$dir/$subdir"
-    ls
-    #bun install
-    #bunx slidev -y -o ../../output/$dir/$subdir --base "/$dir/$subdir" --download true
+    bun install
+    bunx slidev -y -o ../../output/$dir/$subdir --base "/$dir/$subdir" --download true
   done
 done
