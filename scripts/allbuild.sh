44513fcb54b468a87aba04f1a61d40e0675ab736
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-08-10 09:54:15 +0900

    test

--- scripts/allbuild.sh
+++ scripts/allbuild.sh
@@ -24,8 +24,8 @@ for pkg in $(find slides -type f -name package.json); do
 
   target_dir="$ROOT_DIR/output/$rel_path"
   mkdir -p "$target_dir"
-  cp -r "dist/*" "$target_dir/"
   cp -r "static" "$target_dir/static"
+  cp -r dist/* "$target_dir/"
   echo "Copied to $target_dir"
   ls -l "$target_dir"
 
