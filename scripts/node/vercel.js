b69fa54d80032ed728595fcb7ae77a284cb833b1
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-08-10 10:10:32 +0900

    tmiss

--- scripts/node/vercel.js
+++ scripts/node/vercel.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 
 const args = process.argv.slice(2);
-if (args.length < 3) {
-  console.error('使い方: node vercel.js <source> <destination> <statuscode>');
+if (args.length < 2) {
+  console.error('使い方: node vercel.js <source> <destination>');
   process.exit(1);
 }
 
-const [source, destination, statuscode] = args;
+const [source, destination] = args;
 
 const vercelJsonPath = path.resolve(process.cwd(), 'vercel.json');
 
